d737097e28df63aa155a74da1f317a9e5cbfc488
Calderon1711 <alejandro.calderon.campos@est.una.ac.cr>
2025-10-27 19:18:41 -0600

    Mensaje e hilo cliente listos

--- SistemaRecetas-Backend/src/main/java/servidor/red/HiloCliente.java
+++ SistemaRecetas-Backend/src/main/java/servidor/red/HiloCliente.java
@@ -1,4 +1,57 @@
 package servidor.red;
+import servidor.util.ConversorJSON;
+import servidor.constantes.Comandos;
+import servidor.servicio.*; // Para llamar los Service MedicamentoService, PacienteService)
+import servidor.Modelo.*;
+import java.io.*;
+import java.net.Socket;
 
-public class HiloCliente {
+//la funcion de esta clase es que cada vez que el servidor acepta uan conexion de una clientes, crea un nuevo hilo
+
+public class HiloCliente implements Runnable {
+    private final Socket socket;
+    private final BufferedReader in;
+    private final PrintWriter out;
+
+    //contructor
+    public HiloCliente(Socket socket) throws IOException {
+        this.socket = socket;
+        this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.out = new PrintWriter(socket.getOutputStream(), true);
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println("Cliente conectado desde: " + socket.getInetAddress().getHostName());
+
+            //Escuchar mensajes mientrtas el cliente esta conectado
+            String mensajeJSON;
+
+            while((mensajeJSON=in.readLine()) != null){
+             //pasamos el mensaje de JSON a un objeto mensaje
+             Mensaje mensaje= ConversorJSON.deserializar(mensajeJSON, Mensaje.class);
+                System.out.println("comando recibido: "+mensaje.getComando());
+
+                //Procesamos el mensaje segun el comando
+                Mensaje respuesta = procesarMensaje(mensaje);//se crea abajo
+              //enviamos la respuesta al cliente
+            String respuestaJSON= ConversorJSON.serializar(respuesta);
+            out.println(respuestaJSON);
+            }
+        }catch (Exception e){
+            System.out.println("Error en hiloCliente: " + e.getMessage());
+        }finally {
+            try{
+                socket.close();
+                System.out.println("Cliente desconectado: " + socket.getInetAddress());
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private Mensaje procesarMensaje(Mensaje mensaje){
+
+    }
 }
--- SistemaRecetas-Backend/src/main/java/servidor/red/Mensaje.java
+++ SistemaRecetas-Backend/src/main/java/servidor/red/Mensaje.java
@@ -1,4 +1,89 @@
 package servidor.red;
 
-public class Mensaje {
+import java.io.Serializable;
+
+//esta clases es el objeto que viaja entre el fronted y el backend
+public class Mensaje implements Serializable {
+    //Comando o accion que se desea ejecutar por ejemplo "Listar_paciente" o cosas asi
+    private String comando;
+
+    //objeto asociado al comando en este caso el ejemplo seria un paciente a agregar
+    private Object objeto;
+
+    //esto solo indica si la operacion fue existosa o no
+    private boolean exito;
+
+    // mensaje descriptivo en este caso el ejemplo seria Paciente agregado correctamenre
+    private String mensaje;
+
+    //Este es el resultado devuelto en este caso el ejkmplo seria una lista de pacientes
+    private Object resultado;
+
+    //constructores
+
+    /**
+     * Constructor usado por el servidor para responder al cliente.
+     * @param exito   Indica si la operación fue exitosa
+     * @param mensaje Mensaje de respuesta
+     * @param resultado Resultado de la operación (puede ser null)
+     */
+
+    public Mensaje(boolean exito, String mensaje, Object resultado) {
+        this.exito = exito;
+        this.mensaje = mensaje;
+        this.resultado = resultado;
+    }
+
+    //getters y setters
+
+
+    public Object getObjeto() {
+        return objeto;
+    }
+
+    public void setObjeto(Object objeto) {
+        this.objeto = objeto;
+    }
+
+    public String getComando() {
+        return comando;
+    }
+
+    public void setComando(String comando) {
+        this.comando = comando;
+    }
+
+    public boolean isExito() {
+        return exito;
+    }
+
+    public void setExito(boolean exito) {
+        this.exito = exito;
+    }
+
+    public Object getResultado() {
+        return resultado;
+    }
+
+    public void setResultado(Object resultado) {
+        this.resultado = resultado;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    @Override
+    public String toString() {
+        return  "Mensaje{" +
+                "comando='" + comando + '\'' +
+                ", exito=" + exito +
+                ", mensaje='" + mensaje + '\'' +
+                ", objeto=" + objeto +
+                ", resultado=" + resultado +
+                '}';
+    }
 }
